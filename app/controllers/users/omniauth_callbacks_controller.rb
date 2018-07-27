99e3ac8a366645a6172a00eb50da478429d86cc1
Aram Hayrapetyan <aramhrptn@hotmail.com>
2018-07-27 14:31:09 +0400

    initial commit

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,6 +3,13 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env['omniauth.auth'])
     @vk = VkontakteApi::Client.new
 
+
+#    <%= VK::Application.new(app_id: "6642807",
+#                            version: '5.80',
+#                            access_token: current_user.token).getOnline(uid: current_user.uid,
+#                                                                         v: '5.1') %>
+
+    @friends = @vk.friends.get(user_ids: @user.uid, count: "5")
     if @user.persisted?
       sign_in_and_redirect @user, :event => :authentication
       set_flash_message(:notice, :success, :kind => "Vkontakte") if is_navigational_format?
