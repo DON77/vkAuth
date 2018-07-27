eff8cd35d29e24292008c53cf6dbc11851a19901
Aram Hayrapetyan <aramhrptn@hotmail.com>
2018-07-27 13:58:18 +0400

    initial commit

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,6 +1,7 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def vkontakte
     @user = User.from_omniauth(request.env['omniauth.auth'])
+    @vk = VkontakteApi::Client.new
 
     if @user.persisted?
       sign_in_and_redirect @user, :event => :authentication
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
 
   def self.new_with_session(params, session)
     super.tap do |user|
-      if data = session["devise.vkontakte_data"] && session["devise.vkontakte_data"]["extra"]["raw_info"]
+      if data == session["devise.vkontakte_data"] && session["devise.vkontakte_data"]["extra"]["raw_info"]
         user.email = data["email"] if user.email.blank?
       end
     end
@@ -19,6 +19,7 @@ class User < ApplicationRecord
       user.password = Devise.friendly_token[0,20]
       user.name = auth.info.name 
       user.image = auth.info.image
+      user.token = auth.credentials.token
     end
   end
 end
