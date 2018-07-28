3145c04dcbd98d04cd7c2fc8f464eb683ea1f0ef
Aram Hayrapetyan <aramhrptn@hotmail.com>
2018-07-28 20:17:17 +0400

    initial commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,12 +9,13 @@ class User < ApplicationRecord
     super.tap do |user|
       if data == session["devise.vkontakte_data"] && session["devise.vkontakte_data"]["extra"]["raw_info"]
         user.email = data["email"] if user.email.blank?
+
       end
     end
   end
 
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_initialize do |user|
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
       user.name = auth.info.name 
