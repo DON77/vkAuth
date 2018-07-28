d3234cd0f2148a7c10a6e77c0b131a3bcfdb90d9
Aram Hayrapetyan <aramhrptn@hotmail.com>
2018-07-28 20:13:28 +0400

    initial commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ApplicationRecord
   end
 
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create.update do |user|
+    where(provider: auth.provider, uid: auth.uid).first_or_initialize do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
       user.name = auth.info.name 
