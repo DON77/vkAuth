e093453fa1702df0780ce563105a78ff6304503f
Aram Hayrapetyan <aramhrptn@hotmail.com>
2018-07-28 16:10:10 +0400

    initial commit

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -289,11 +289,11 @@ Devise.setup do |config|
     lang: 'ru',
     https: 1
 
-  Warden::Manager.after_authentication do |user, auth, opts|
-    if user.remember_me
-      auth.cookies[:email] = {value: user.email, expires: 2.weeks.from_now}
-    else
-      auth.cookies.delete :email
-    end
+  #Warden::Manager.after_authentication do |user, auth, opts|
+  #  if user.remember_me
+  #    auth.cookies[:email] = {value: user.email, expires: 2.weeks.from_now}
+  #  else
+  #    auth.cookies.delete :email
+  #  end
   end
 end
